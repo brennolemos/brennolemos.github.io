825e8ce92937cf24e11c8acbe2adfa6c64c47cc9
brennolemos <brenomiros@gmail.com>
2017-02-21 10:57:55 -0300

    Insere scroll no btn de portfolio

--- js/app.js
+++ js/app.js
@@ -7,3 +7,13 @@ $('nav a').click(function(e){
         scrollTop: targetOffset - menuHeight
     }, 500)
 });
+
+$('.introducao a').click(function(e){
+    e.preventDefault();
+    var id = $(this).attr('href'),
+        targetOffset = $(id).offset().top,
+        menuHeight = $('nav').innerHeight();
+    $('html, body').animate({
+        scrollTop: targetOffset - menuHeight
+    }, 500)
+});
