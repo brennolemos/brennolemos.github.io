e5e8cf4926e2bf90175cbb8d36dfaa2df3db68c5
brennolemos <brenomiros@gmail.com>
2017-02-21 22:14:53 -0300

    Torna a página responsiva e finaliza as animações

--- js/app.js
+++ js/app.js
@@ -17,3 +17,21 @@ $('.introducao a').click(function(e){
         scrollTop: targetOffset - menuHeight
     }, 500)
 });
+
+Visibility.onVisible(function(){
+    setTimeout(function() {
+        $(".introducao h1").addClass("animated fadeInDown")
+    }, 400);
+    
+     setTimeout(function() {
+        $("blockquote").addClass("animated fadeInDown")
+    }, 800);
+    
+     setTimeout(function() {
+        $(".introducao .btn").addClass("animated fadeInDown")
+    }, 1200);
+    
+    setTimeout(function() {
+        $(".animar").addClass("animated fadeInDown")
+    }, 1600);
+});
