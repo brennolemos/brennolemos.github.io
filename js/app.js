af2f5154c6cbeca2c3d919be0e5f5fe55b4d0d48
brennolemos <brenomiros@gmail.com>
2017-03-26 22:49:15 -0300

    Concerta o scroll suave (jquery)

--- js/app.js
+++ js/app.js
@@ -26,34 +26,31 @@ $(document).ready(function(){
         $('.header').css({"background-color": "transparent"});
       }
   });
-  
+
 
   function filterPath(string) {
-  return string
-  .replace(/^\//,'')
-  .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
-  .replace(/\/$/,'');
-  }
-  $('a[href*=#]').each(function() {
-  if ( filterPath(location.pathname) == filterPath(this.pathname)
-  && location.hostname == this.hostname
-  && this.hash.replace(/#/,'') ) {
-  var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
-  var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
-  if ($target) {
-  var targetOffset = $target.offset().top;
-  $(this).click(function() {
-  $('html, body').animate({scrollTop: targetOffset}, 400);
-  return false;
-  });
-  }
-  }
+      return string
+      .replace(/^\//,'')
+      .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
+      .replace(/\/$/,'');
+    }
+    $('a[href*=#]').each(function() {
+      if ( filterPath(location.pathname) == filterPath(this.pathname)
+      && location.hostname == this.hostname
+      && this.hash.replace(/#/,'') ) {
+        var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
+        var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
+        if ($target) {
+          var targetOffset = $target.offset().top;
+          $(this).click(function() {
+          $('html, body').animate({scrollTop: targetOffset}, 400);
+          return false;
+          });
+        }
+      }
   });
 
-  
+
   // Mudar para active o menu de acordo com a área
- 
+
 });
-
-
-
