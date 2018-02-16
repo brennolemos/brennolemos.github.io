9f6d7ddbcbb101ce95a78437f5ae501061a06407
brennolemos <brenomiros@gmail.com>
2018-02-15 21:57:08 -0300

    Add Bootstrap

--- js/app.js
+++
@@ -1,8 +0,0 @@
-    $(document).ready(function(){
-
-        $('.mobile-btn').click(function(){
-            $(this).toggleClass('active');
-            $('.mobile-menu').toggleClass('active');
-        });
-        
-    });
