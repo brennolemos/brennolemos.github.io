afec966e993d09e51275e78d0b9ac17611b952c5
brennolemos <brenomiros@gmail.com>
2018-12-03 22:45:56 -0300

    Adiciona Gulp Autoprefizer e package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 function compilaSass() {
   return gulp.src('css/scss/**/*.scss')
-  .pipe(sass({ outputStyle: 'compressed' }))
-  .pipe(gulp.dest('css/'))
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('css/'))
 }
 
 gulp.task('sass', compilaSass);
