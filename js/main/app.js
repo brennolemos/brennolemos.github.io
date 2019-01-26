22e38bf82696632d2c24b1b7ef6e00211ca3046e
Breno Lemos <brenomiros@gmail.com>
2019-01-26 12:47:56 -0200

    Navegação SPA via Ajax na Home

--- js/main/app.js
+++ js/main/app.js
@@ -1,5 +1,4 @@
 // Menu Mobile
-
 const menuButton = document.querySelector('.menu-mobile-btn');
 const menuList = document.querySelector('.itens-paginas.mobile');
 
@@ -9,3 +8,51 @@ const openMenu = (event) => {
 }
 
 menuButton.addEventListener('click', openMenu);
+
+
+// Navegação SPA Ajax
+(function () {
+    function marcarLinkComoSelecionado(hash) {
+        const links = document.querySelectorAll(`[wm-link]`)
+        links.forEach(link => link.classList.remove('selecionado'))
+
+        const link = document.querySelector(`[wm-link='${hash}']`)
+        link.classList.add('selecionado')
+    }
+
+    function navegarViaAjax(hash) {
+        if (!hash) return
+
+        const link = document.querySelector(`[wm-link='${hash}']`)
+        const destino = document.querySelector('[wm-link-destino]')
+
+        const url = hash.substring(1)
+        fetch(url)
+            .then(resp => resp.text())
+            .then(html => {
+                destino.innerHTML = html
+                marcarLinkComoSelecionado(hash)
+            })
+    }
+
+    function configurarLinks() {
+        document.querySelectorAll('[wm-link]')
+            .forEach(link => {
+                link.href = link.attributes['wm-link'].value
+            })
+    }
+
+    function navegacaoInicial() {
+        if (location.hash) {
+            navegarViaAjax(location.hash)
+        } else {
+            const primeiroLink = document.querySelector('[wm-link]')
+            navegarViaAjax(primeiroLink.hash)
+        }
+    }
+
+    window.onhashchange = e => navegarViaAjax(location.hash)
+    
+    configurarLinks()
+    navegacaoInicial()
+})()
