6379a29d31816b0cae27089ae1185d75f08c7dfc
Breno Lemos <brenomiros@gmail.com>
2021-04-18 16:34:05 -0300

    Migrates skills component for TypeScript

--- src/Components/Skills/Skills.tsx
+++ src/Components/Skills/Skills.tsx
@@ -2,10 +2,19 @@ import React from 'react';
 
 import * as S from './styles';
 
-const Skills = (props) => {
+type SkillsProps = {
+  skills: Skill[];
+};
+
+type Skill = {
+  id: string;
+  icon: string;
+};
+
+const Skills = ({ skills }: SkillsProps) => {
   return (
     <div className="row my-4">
-      {props.skills.map((skill) => (
+      {skills.map((skill) => (
         <div key={skill.id} className="col-md-3 mb-4">
           <S.IconContainer>
             <i className={skill.icon}></i>
