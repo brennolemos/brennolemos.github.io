7a114e93d9db04f3fa2c2eed0837ab277ff30b25
Breno Lemos <brenomiros@gmail.com>
2021-04-18 16:29:34 -0300

    Migrates skills component styles for styled components

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import "./skills.css";
+import React from 'react';
+
+import * as S from './styles';
 
 const Skills = (props) => {
   return (
     <div className="row my-4">
       {props.skills.map((skill) => (
         <div key={skill.id} className="col-md-3 mb-4">
-          <div className="icon-skill mx-auto">
+          <S.IconContainer>
             <i className={skill.icon}></i>
-          </div>
+          </S.IconContainer>
         </div>
       ))}
     </div>
-    
   );
 };
 
--- src/Components/Skills/styles.ts
+++ src/Components/Skills/styles.ts
@@ -1,5 +1,6 @@
-/* Skills */
-.icon-skill {
+import styled from 'styled-components';
+
+export const IconContainer = styled.div`
   background-color: #cbdc3d;
   width: 120px;
   height: 120px;
@@ -7,6 +8,7 @@
   display: flex;
   justify-content: center;
   align-items: center;
-  /* // font-size: 28px; */
   color: #2d3940;
-}
+  margin-left: auto;
+  margin-right: auto;
+`;
