3d298d0cad5deb39c23487a9ab5c7b859eed1cbb
Breno Lemos <brenomiros@gmail.com>
2021-04-18 17:17:56 -0300

    Migrates timeline component for TypeScript

--- src/Components/Timeline/Timeline.tsx
+++ src/Components/Timeline/Timeline.tsx
@@ -2,15 +2,30 @@ import React from 'react';
 
 import * as S from './styles';
 
-export default (props) => {
+type Place = {
+  id: string;
+  periodo: string;
+  ocupacao: string;
+  titulo: string;
+};
+
+type TimelineProps = {
+  infos: {
+    titulo: string;
+    icon: string;
+    list: Place[];
+  };
+};
+
+export default ({ infos }: TimelineProps) => {
   return (
     <>
-      <h2>{props.infos.titulo}</h2>
+      <h2>{infos.titulo}</h2>
       <S.Timeline>
-        {props.infos.list.map((item) => (
+        {infos.list.map((item) => (
           <S.Item key={item.id}>
             <S.Icon>
-              <i className={props.infos.icon}></i>
+              <i className={infos.icon}></i>
             </S.Icon>
             <div className="timeline__infos">
               <S.Period>{item.periodo}</S.Period>
